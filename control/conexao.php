7314cf332223194116306e6bf94bd080d6004f50
Guilherme Curiel <guilherme.curiel@gmail.com>
2026-04-14 15:22:10 -0300

    Atualização: correção de conexão

--- control/conexao.php
+++ control/conexao.php
@@ -1,9 +1,13 @@
 <?php
 
-//Realiza conexão com o Banco de Dados
-$conn = new mysqli("db", "u3_user", "admin", "U3");
+// XAMPP (acesso de qualquer máquina - se o seu root tiver senha, substituir o "" com a senha)
+$conn = new mysqli("localhost", "root", "", "U3");
 
-//Se caso der erro, mostra em tela
+//$conn = new mysqli("localhost", "user", "password", "U3"); > para máquinas que tenham criado usr e passwd para acessar a Base
+
+// Docker (usar se necessário)
+// $conn = new mysqli("db", "u3_user", "***", "U3"); > minha máquina
+
 if ($conn->connect_error) {
     die("Erro: " . $conn->connect_error);
 }
