76213756c4375cbfe29aa9d207070b6f90dc447c
Guilherme Curiel <guilherme.curiel@gmail.com>
2026-05-05 20:25:59 -0300

    Adicionando pipeline CI/CD

---
+++ README.txt.txt
@@ -0,0 +1,5 @@
+Como não foi realizado o Doc, estou descrevendo os integrantes da equipe abaixo:
+
+Guilherme Henrique Curiel Caetano [318239]
+Leonardo Astolpho dos Santos [318269]
+Melissa Nathalia Cuenca Zaldivar [318261]
--- control/conexao.php
+++ control/conexao.php
@@ -1,12 +1,12 @@
 <?php
 
 // XAMPP (acesso de qualquer máquina - se o seu root tiver senha, substituir o "" com a senha)
-$conn = new mysqli("localhost", "root", "", "U3");
+//$conn = new mysqli("localhost", "root", "", "U3");
 
 //$conn = new mysqli("localhost", "user", "password", "U3"); > para máquinas que tenham criado usr e passwd para acessar a Base
 
 // Docker (usar se necessário)
-// $conn = new mysqli("db", "u3_user", "***", "U3"); > minha máquina
+$conn = new mysqli("db", "u3_user", "***", "U3"); //> minha máquina
 
 if ($conn->connect_error) {
     die("Erro: " . $conn->connect_error);
